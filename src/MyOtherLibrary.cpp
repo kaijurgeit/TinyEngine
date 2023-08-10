e489d12b5a339fb241961c626fd6b694a78bd20c
Kai Jurgeit <kai.jurgeit@t-online.de>
2023-08-10 13:22:58 +0200

    CMake and basic dependencies added to build porject with OpenGL, resolves #3.

---
+++ src/MyOtherLibrary.cpp
@@ -0,0 +1,9 @@
+#include "MyOtherlibrary.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int TE::Foo()
+{
+    return 49;
+}
---
+++ src/MyOtherLibrary.h
@@ -0,0 +1,7 @@
+#ifndef MY_OTHER_LIBRARY_H
+#define MY_OTHER_LIBRARY_H
+namespace TE
+{
+    int Foo();
+}
+#endif // MY_OTHER_LIBRARY_H
