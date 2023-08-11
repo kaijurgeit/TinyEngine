c77585da84c70d6bf2124719d70104e56e0227fc
Kai Jurgeit <kai.jurgeit@t-online.de>
2023-08-11 11:33:17 +0200

    Linking error.

--- src/TinyEngine.h
+++ src/TinyEngine.h
@@ -1,5 +1,9 @@
 #pragma once
 
 #include "Application.h"
-
 #include "Main.h"
+
+// namespace TE
+// {
+//     bool DllImportTest() { return true; }
+// }
--- test/Init_Test.cpp
+++ test/Init_Test.cpp
@@ -1,6 +1,12 @@
 #include <gtest/gtest.h>
+#include <TinyEngine.h>
 
 TEST(InitTests, Basic)
 {
     EXPECT_TRUE(true);
 }
+
+// TEST(InitTests, DllImport)
+// {
+//     EXPECT_TRUE(true);
+// }
